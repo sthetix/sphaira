22a34d142cd514791b240ab1f730e4c51d63058e
sthetix <cs@sthetix.info>
2026-04-19 18:14:20 +0700

    Make libusbdvd patch work across libnx versions

--- cmake/patch_libusbdvd.cmake
+++ cmake/patch_libusbdvd.cmake
@@ -23,6 +23,24 @@ enum usb_request_type {
 ]])
 
 if(content MATCHES "enum usb_request_recipient")
-    string(REPLACE "${old_block}" "" content "${content}")
+    set(new_block [[enum usbdvd_request_recipient {
+    USBDVD_USB_RECIPIENT_DEVICE    = 0x00,
+    USBDVD_USB_RECIPIENT_INTERFACE = 0x01,
+    USBDVD_USB_RECIPIENT_ENDPOINT  = 0x02,
+    USBDVD_USB_RECIPIENT_OTHER     = 0x03,
+};
+
+enum usbdvd_request_type {
+    USBDVD_USB_REQUEST_TYPE_STANDARD = (0x00 << 5),
+    USBDVD_USB_REQUEST_TYPE_CLASS    = (0x01 << 5),
+    USBDVD_USB_REQUEST_TYPE_VENDOR   = (0x02 << 5),
+    USBDVD_USB_REQUEST_TYPE_RESERVED = (0x03 << 5),
+};
+]])
+    string(REPLACE "${old_block}" "${new_block}" content "${content}")
+    string(REPLACE "USB_REQUEST_TYPE_CLASS" "USBDVD_USB_REQUEST_TYPE_CLASS" content "${content}")
+    string(REPLACE "USB_RECIPIENT_INTERFACE" "USBDVD_USB_RECIPIENT_INTERFACE" content "${content}")
+    string(REPLACE "USB_REQUEST_TYPE_STANDARD" "USBDVD_USB_REQUEST_TYPE_STANDARD" content "${content}")
+    string(REPLACE "USB_RECIPIENT_ENDPOINT" "USBDVD_USB_RECIPIENT_ENDPOINT" content "${content}")
     file(WRITE "${FILE}" "${content}")
 endif()

998cccae3abddadd7854346759c514fed5a730e8
sthetix <cs@sthetix.info>
2026-04-19 18:17:23 +0700

    Narrow libusbdvd compatibility patch

--- cmake/patch_libusbdvd.cmake
+++ cmake/patch_libusbdvd.cmake
@@ -1,13 +1,10 @@
-# Keep libusbdvd compatible with newer libnx releases that already define
-# usb_request_type and usb_request_recipient.
-
 if(NOT DEFINED FILE)
     message(FATAL_ERROR "FILE must be set")
 endif()
 
 file(READ "${FILE}" content)
 
-set(old_block [[enum usb_request_recipient {
+set(old_enum_block [[enum usb_request_recipient {
     USB_RECIPIENT_DEVICE    = 0x00,
     USB_RECIPIENT_INTERFACE = 0x01,
     USB_RECIPIENT_ENDPOINT  = 0x02,
@@ -22,8 +19,7 @@ enum usb_request_type {
 };
 ]])
 
-if(content MATCHES "enum usb_request_recipient")
-    set(new_block [[enum usbdvd_request_recipient {
+set(local_enum_block [[enum usbdvd_request_recipient {
     USBDVD_USB_RECIPIENT_DEVICE    = 0x00,
     USBDVD_USB_RECIPIENT_INTERFACE = 0x01,
     USBDVD_USB_RECIPIENT_ENDPOINT  = 0x02,
@@ -37,10 +33,27 @@ enum usbdvd_request_type {
     USBDVD_USB_REQUEST_TYPE_RESERVED = (0x03 << 5),
 };
 ]])
-    string(REPLACE "${old_block}" "${new_block}" content "${content}")
-    string(REPLACE "USB_REQUEST_TYPE_CLASS" "USBDVD_USB_REQUEST_TYPE_CLASS" content "${content}")
-    string(REPLACE "USB_RECIPIENT_INTERFACE" "USBDVD_USB_RECIPIENT_INTERFACE" content "${content}")
-    string(REPLACE "USB_REQUEST_TYPE_STANDARD" "USBDVD_USB_REQUEST_TYPE_STANDARD" content "${content}")
-    string(REPLACE "USB_RECIPIENT_ENDPOINT" "USBDVD_USB_RECIPIENT_ENDPOINT" content "${content}")
-    file(WRITE "${FILE}" "${content}")
+
+if(content MATCHES "enum usb_request_recipient")
+    string(REPLACE "${old_enum_block}" "" content "${content}")
 endif()
+
+set(anchor [[enum usb_request_bot {
+    USB_REQUEST_BOT_GET_MAX_LUN = 0xFE,
+    USB_REQUEST_BOT_RESET       = 0xFF
+};
+]])
+
+if(NOT content MATCHES "enum usbdvd_request_recipient")
+    string(REPLACE "${anchor}" "${anchor}\n${local_enum_block}" content "${content}")
+endif()
+
+set(old_reset [[    rc = usbHsIfCtrlXfer(usb_if_session, USB_ENDPOINT_OUT | USB_REQUEST_TYPE_CLASS | USB_RECIPIENT_INTERFACE, USB_REQUEST_BOT_RESET, 0, if_num, 0, NULL, &xfer_size);]])
+set(new_reset [[    rc = usbHsIfCtrlXfer(usb_if_session, USB_ENDPOINT_OUT | USBDVD_USB_REQUEST_TYPE_CLASS | USBDVD_USB_RECIPIENT_INTERFACE, USB_REQUEST_BOT_RESET, 0, if_num, 0, NULL, &xfer_size);]])
+string(REPLACE "${old_reset}" "${new_reset}" content "${content}")
+
+set(old_clear [[    rc = usbHsIfCtrlXfer(usb_if_session, USB_ENDPOINT_OUT | USB_REQUEST_TYPE_STANDARD | USB_RECIPIENT_ENDPOINT, USB_REQUEST_CLEAR_FEATURE, 0x00, ep_addr, 0, NULL, &xfer_size);]])
+set(new_clear [[    rc = usbHsIfCtrlXfer(usb_if_session, USB_ENDPOINT_OUT | USBDVD_USB_REQUEST_TYPE_STANDARD | USBDVD_USB_RECIPIENT_ENDPOINT, USB_REQUEST_CLEAR_FEATURE, 0x00, ep_addr, 0, NULL, &xfer_size);]])
+string(REPLACE "${old_clear}" "${new_clear}" content "${content}")
+
+file(WRITE "${FILE}" "${content}")
